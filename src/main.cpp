e1e7d8057fd1ee50022a5af74f8441d7fb7d49a8
Ionut Cotoi <icotoi@gmail.com>
2015-05-26 22:05:54 +0300

    removed timer test code

--- src/main.cpp
+++ src/main.cpp
@@ -8,57 +8,6 @@
 using namespace std;
 using namespace devicehub;
 
-#if __cplusplus < 201103L && (!defined(_MSC_VER) || _MSC_VER < 1700)
-#error jrd::Timer class requires C++11 support
-#else
-#include <chrono>
-#include <ostream>
-
-namespace jrd {
-    namespace time {
-        class Timer {
-            typedef std::chrono::high_resolution_clock high_resolution_clock;
-            typedef std::chrono::milliseconds milliseconds;
-        public:
-            explicit Timer(bool run = false)
-            {
-                if (run)
-                    Reset();
-            }
-            void Reset()
-            {
-                _start = high_resolution_clock::now();
-            }
-            milliseconds Elapsed() const
-            {
-                return std::chrono::duration_cast<milliseconds>(high_resolution_clock::now() - _start);
-            }
-            template <typename T, typename Traits>
-            friend std::basic_ostream<T, Traits>& operator<<(std::basic_ostream<T, Traits>& out, const Timer& timer)
-            {
-                return out << timer.Elapsed().count();
-            }
-        private:
-            high_resolution_clock::time_point _start;
-        };
-    }
-}
-#endif
-
-void timer_test() {
-    jrd::time::Timer timer(true);
-    // Kill some time
-    for (int i = 0; i < 1000000000; i++)
-        ;
-    std::cout << "Elapsed time: " << std::fixed << timer << "ms\n";
-    timer.Reset();
-    // Kill some more time
-    for (int i = 0; i < 10000000; i++)
-        ;
-    auto elapsed = timer.Elapsed();
-    std::cout << "Elapsed time: " << std::fixed << elapsed.count() << "ms\n";
-}
-
 // Project API key
 api_key_t     myApiKey    = "a8b274d9-4484-4103-8214-e6db353abbd9";
 // Project ID
